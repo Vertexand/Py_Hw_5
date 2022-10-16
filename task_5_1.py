7245464353d61265d4bbb264a154f053b7faf47f
Natalli <MobileVersion.Nataliayandx1212@yandex.by>
2022-10-16 07:30:18 +0300

    add task

---
+++ task_5_1.py
@@ -0,0 +1,9 @@
+# 1. Напишите программу, удаляющую из текста все слова, содержащие "абв".
+
+# text = 'Съешь ещёабв этих мягабвких французских булок абв'
+# my_list = list(filter(lambda x: 'абв' not in x , text.split()))
+# print(' '.join(my_list))
+
+
+a= filter(lambda x: ('абв' not in x),  ('dabvf abvlj abv kjkdabv'))
+print(list(a))

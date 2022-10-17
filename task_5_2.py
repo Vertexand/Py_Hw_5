11ffca161a0f53a04320f35f004a0272f2494e75
Natalli <MobileVersion.Nataliayandx1212@yandex.by>
2022-10-17 20:31:36 +0300

    add 17.10.22

--- task_5_2.py
+++ task_5_2.py
@@ -1,31 +1,53 @@
-# 2. Дан список чисел. Создайте список, в который попадают числа,
-#    описываемые возрастающую последовательность.
-#    Порядок элементов менять нельзя.
+# 2. Создайте программу для игры с конфетами человек против человека.
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. 
+# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
 
-from random import choices
+# a) Добавьте игру против бота
 
+# b) Подумайте как наделить бота ""интеллектом""
 
-def sequ(num):
-    if num < 1:
-        return []
-    return choices(range(num * 2), k=num)
+from random import shuffle, randint
 
-
-def all_sets(num_list):
-    new_list = []
-    for k in range(len(num_list)):
-        n = num_list[k]
-        temporary = [n]
-        for i in range(k + 1, len(num_list)):
-            if num_list[i] > n:
-                n = num_list[i]
-                temporary.append(n)
-        if len(temporary) > 1:
-            new_list.append(temporary)
-
-    return new_list
+# CANDIES = 2021
+CANDIES = 100
+CANDIES_LIMIT = 28
 
 
-list_nums = sequ(int(input()))
-print(list_nums)
-print(all_sets(list_nums))
+def bot_run(candies: int) -> int:
+    result = candies % 29
+    if not result:
+        result = randint(1, 28)
+    return result
+
+
+def moves(pl_act):
+    first, second = players
+    return second if pl_act == first else first
+
+
+players = ["human", 'bot' if int(input('Play with bot 1 - yes, 0 - no? ')) else 'person']
+shuffle(players)
+
+active_player = players[0]
+print(f'1 player - {players[0]}, 2 player - {players[1]}')
+
+while CANDIES > 0:
+    print(f'\nThere are {CANDIES} sweets on the table, you can take [1 .. {CANDIES_LIMIT}]')
+    print(f"Player {active_player}'s move")
+
+    if active_player == "bot":
+        get_candies = bot_run(CANDIES)
+        print(f'The bot took {get_candies} candies')
+    else:
+        get_candies = int(input(f'How many candies do you want {active_player}: '))
+
+    # проверки
+    if get_candies not in range(1, CANDIES_LIMIT + 1):
+        print('Wrong move!')
+    else:
+        CANDIES -= get_candies
+        if CANDIES > 0:
+            active_player = moves(active_player)
+        else:
+            print(f'The player {active_player} won!')
+

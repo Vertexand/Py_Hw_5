c89d55b7a6282f66038368bfe18139896fad42ca
Natalli <MobileVersion.Nataliayandx1212@yandex.by>
2022-10-17 20:49:38 +0300

    add 17/10/22

--- task_5_1.py
+++ task_5_1.py
@@ -1,9 +1,12 @@
 # 1. Напишите программу, удаляющую из текста все слова, содержащие "абв".
 
-# text = 'Съешь ещёабв этих мягабвких французских булок абв'
-# my_list = list(filter(lambda x: 'абв' not in x , text.split()))
-# print(' '.join(my_list))
+text = 'Съешь ещёабв этих мягабвких французских булок абв'
+my_list = list(filter(lambda x: 'абв' not in x , text.split()))
+print(' '.join(my_list))
 
 
-a= filter(lambda x: ('абв' not in x),  ('dabvf abvlj abv kjkdabv'))
-print(list(a))
+
+
+
+# a= filter(lambda x: ('абв' not in x),  ('dabvf abvlj abv kjkdabv'))
+# print(list(a))
